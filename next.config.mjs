f109246d4ddbcd449ded01e519ab809d5484de52
Aditya <adityagundg14@gmail.com>
2024-06-27 22:16:56 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-// next.config.mjs
-
 import path from 'path';
 
 const nextConfig = {
   webpack(config) {
     config.resolve.alias['@firebaseConfig'] = path.resolve('./firebaseConfig.js');
     return config;
+  },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
 };
 
